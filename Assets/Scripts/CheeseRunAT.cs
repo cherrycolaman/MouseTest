29b2f584ba5a5b68cfc734c536af4670837fc2fd
cherrycolaman <148461991+cherrycolaman@users.noreply.github.com>
2025-02-22 19:30:20 -0500

    Mouse moves to cheese

---
+++ Assets/Scripts/CheeseRunAT.cs
@@ -0,0 +1,43 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine.AI;
+using UnityEngine;
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class CheeseRunAT : ActionTask {
+        private NavMeshAgent navAgent;
+        public BBParameter<GameObject> cheese;
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
+            navAgent = agent.GetComponent<NavMeshAgent>();
+            return null;
+		}
+
+		//This is called once each time the task is enabled.
+		//Call EndAction() to mark the action as finished, either in success or failure.
+		//EndAction can be called from anywhere.
+		protected override void OnExecute() {
+			navAgent.SetDestination(cheese.value.transform.position);
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+            if (!navAgent.pathPending && navAgent.remainingDistance <= navAgent.stoppingDistance)
+            {
+                EndAction(true);
+            }
+        }
+
+		//Called when the task is disabled.
+		protected override void OnStop() {
+			
+		}
+
+		//Called when the task is paused.
+		protected override void OnPause() {
+			
+		}
+	}
+}
